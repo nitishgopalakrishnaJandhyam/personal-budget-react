89524765d494d6f1ad507354879531408e21b8e2
nitishgopalakrishnaJandhyam <njandhya@charlotte.edu>
2024-09-30 00:49:33 -0400

    addded charts

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer" role="contentinfo">
+        <div className="center">
+            All rights reserved &copy; nitish jandhaym
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
